75961c0b66b66bf493b2f9b46666765064a56454
Maur Zac <maur.mx@gmail.com>
2021-12-18 23:26:33 -0600

    cambios menores

--- UpaxAppExamen/ViewController.swift
+++ UpaxAppExamen/ViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    
+    @IBOutlet var table : UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello Cell"
+        return cell
     }
 
 
